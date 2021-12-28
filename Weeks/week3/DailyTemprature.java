ce252623afe6d8f6aec7761bf5dc2d0045d96f04
samiya <infinityou111@gmail.com>
2021-12-28 23:32:49 +0300

    leetcode daily temp soln

---
+++ Weeks/week3/DailyTemprature.java
@@ -0,0 +1,17 @@
+public class DailyTemprature {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> dailyTemp = new Stack();
+        int[] answer = new int[temperatures.length];
+        for(int i = 0; i <temperatures.length; i++){
+                while(!dailyTemp.isEmpty() && temperatures[i]>temperatures[dailyTemp.peek()]){
+                    int popped = dailyTemp.pop();
+                    answer[popped] = i - popped;  
+                }
+             
+            
+              dailyTemp.push(i);
+        }
+         return answer;   
+        
+    }
+}
