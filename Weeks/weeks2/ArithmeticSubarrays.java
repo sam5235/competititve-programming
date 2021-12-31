c9c1f1d3f224bc16b30b4af3d8d0ac49feb6b3be
samiya <infinityou111@gmail.com>
2021-12-31 23:38:23 +0300

    leetcode Arthmetic subarray soln

---
+++ Weeks/weeks2/ArithmeticSubarrays.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ArithmeticSubarrays {
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> bool = new ArrayList<>();
+       
+        for (int k = 0; k < l.length ; k++) {
+            List<Integer> subArth = new ArrayList<>();
+            for (int i = l[k]; i <= r[k]; i++) {
+                subArth.add(nums[i]);
+            }
+            Collections.sort(subArth);
+            int diff = subArth.get(1) - subArth.get(0);
+            boolean val  = true;
+            for (int i = 0; i < subArth.size()-1; i++) {
+                if (subArth.get(i + 1) - subArth.get(i) != diff) {
+                  val = false;
+                    break;
+                }
+            
+            }
+            bool.add(val);
+        }
+        return bool;
+    }
+}
