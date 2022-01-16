2409d3cc20612354d0c84d010b92a692d36af1b7
samiya <infinityou111@gmail.com>
2022-01-16 22:05:07 +0300

    hackerrank contest soln

---
+++ Weeks/contest/DivisibleSumPairs.java
@@ -0,0 +1,16 @@
+public class DivisibleSumPairs {
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        // Write your code here
+            int counter = 0;
+            for(int i = 0; i < n -1; i++){
+             
+                for( int j = i + 1; j < n; j++){
+                    if((ar.get(i) + ar.get(j)) % k == 0){
+                        counter++;
+                    }
+                }
+            }
+            
+        return counter;
+        }
+}
